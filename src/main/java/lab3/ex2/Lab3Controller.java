30dfff54f942a45e096993c4a5b1b5e1dac6f8b0
Junrong Li <leejr@uw.edu>
2019-03-03 16:16:23 +0800

    initial setup

---
+++ src/main/java/lab3/ex2/Lab3Controller.java
@@ -0,0 +1,31 @@
+package lab3.ex2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class Lab3Controller {
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private TextField textfield1;
+
+    @FXML
+    void buttonPressed(ActionEvent event) {
+    	label1.setText(textfield1.getText());
+    }
+
+    @FXML
+    void textInput(KeyEvent event) {
+    	label1.setText(textfield1.getText()+" xixi");
+    }
+
+}
